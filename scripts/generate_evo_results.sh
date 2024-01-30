2c128887cd4f8eea616fcb463a837a9dc1cce7d3
Andreas Serov <serov@uni-bremen.de>
2024-01-30 14:45:46 +0100

    removed some comments from evo bash script

--- scripts/generate_evo_results.sh
+++ scripts/generate_evo_results.sh
@@ -16,28 +16,12 @@ robot_folder=$(dirname $result_trajectory_file)
 
 # Execute evo_ape commands
 ape_pose_result_file="${robot_folder}/${robot_name}_ape.zip"
-# ape_trans_reuslt_file="${robot_folder}/${robot_name}_trans_ape.zip"
-# ape_rot_result_file="${robot_folder}/${robot_name}_rot_ape.zip"
 cmd="evo_ape tum $ground_truth_file $result_trajectory_file --align --plot --plot_mode xy --save_results $ape_pose_result_file"
 echo $cmd
 eval $cmd
-# cmd="evo_ape tum $ground_truth_file $result_trajectory_file -r trans_part --align --plot --plot_mode xy --save_results $ape_trans_reuslt_file"
-# echo $cmd
-# eval $cmd
-# cmd="evo_ape tum $ground_truth_file $result_trajectory_file -r rot_part --align --plot --plot_mode xy --save_results $ape_rot_result_file"
-# echo $cmd
-# eval $cmd
 
 # Execute evo_rpe commands
 rpe_result_file="${robot_folder}/${robot_name}_rpe.zip"
-# rpe_trans_result_file="${robot_folder}/${robot_name}_trans_rpe.zip"
-# rpe_rot_result_file="${robot_folder}/${robot_name}_rot_rpe.zip"
 cmd="evo_rpe tum $ground_truth_file $result_trajectory_file --align --plot --plot_mode xy --save_results $rpe_result_file"
 echo $cmd
 eval $cmd
-# cmd="evo_rpe tum $ground_truth_file $result_trajectory_file -r trans_part --align --plot --plot_mode xy --save_results $rpe_trans_result_file"
-# echo $cmd
-# eval $cmd
-# cmd="evo_rpe tum $ground_truth_file $result_trajectory_file -r rot_part --align --plot --plot_mode xy --save_results $rpe_rot_result_file"
-# echo $cmd
-# eval $cmd
