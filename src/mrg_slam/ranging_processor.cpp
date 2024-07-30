77cb44c69681267913a4c9ad4c1ade25077da91c
sero_an <serov@uni-bremen.de>
2024-07-30 14:22:18 +0200

    loop closure radius is now correctly displayed as circle in the xy plane

--- src/mrg_slam/ranging_processor.cpp
+++ src/mrg_slam/ranging_processor.cpp
@@ -11,17 +11,17 @@ RangingProcessor::onInit( rclcpp::Node::SharedPtr _node )
 
     own_name = node->get_parameter( "own_name" ).as_string();
 
-    ranging_topic              = node->declare_parameter( "ranging_topic", "/ranging_measurement" );
-    ranging_position_topic     = node->declare_parameter( "ranging_position_topic", "/gnss_rtk_rel_nav" );
-    ranging_names              = node->declare_parameter( "ranging_names", std::vector<std::string>() );
-    ranging_position_stddev    = node->declare_parameter( "ranging_position_stddev", 0.05 );
-    ranging_orientation_stddev = node->declare_parameter( "ranging_orientation_stddev", 0.1 );
-    ranging_max_time_diff      = node->declare_parameter( "ranging_max_time_diff", 0.05 );
-    // multi_robot_names      = node->get_parameter( "multi_robot_names" ).as_string_array();
+    ranging_topic                   = node->declare_parameter( "ranging_topic", "/ranging_measurement" );
+    ranging_position_topic          = node->declare_parameter( "ranging_position_topic", "/gnss_rtk_rel_nav" );
+    ranging_names                   = node->declare_parameter( "ranging_names", std::vector<std::string>() );
+    ranging_position_stddev         = node->declare_parameter( "ranging_position_stddev", 0.05 );
+    ranging_orientation_stddev      = node->declare_parameter( "ranging_orientation_stddev", 0.1 );
+    ranging_max_time_diff           = node->declare_parameter( "ranging_max_time_diff", 0.05 );
+    ranging_edge_robust_kernel      = node->declare_parameter( "ranging_edge_robust_kernel", "NONE" );
+    ranging_edge_robust_kernel_size = node->declare_parameter( "ranging_edge_robust_kernel_size", 1.0 );
 
     std::string prefix = own_name.empty() ? "" : "/" + own_name;
     ranging_topic      = prefix + ranging_topic;
-
     RCLCPP_INFO_STREAM( node->get_logger(), "Subscribing to ranging topic: " << ranging_topic );
     ranging_sub = node->create_subscription<ros2_radio_ranging_interfaces::msg::Range>( ranging_topic, rclcpp::QoS( 10 ),
                                                                                         std::bind( &RangingProcessor::ranging_callback,
@@ -37,7 +37,7 @@ RangingProcessor::onInit( rclcpp::Node::SharedPtr _node )
 }
 
 void
-RangingProcessor::position_callback( const nav_msgs::msg::Odometry::SharedPtr position_msg )
+RangingProcessor::position_callback( nav_msgs::msg::Odometry::ConstSharedPtr position_msg )
 {
     for( const auto &name : ranging_names ) {
         if( position_msg->child_frame_id.find( name ) != std::string::npos && !ranging_data_map[name].initialized ) {
@@ -64,42 +64,20 @@ RangingProcessor::flush( std::shared_ptr<GraphSLAM> &graph_slam, const std::vect
 
     init_positions( graph_slam );
 
-    double min_time_diff = std::numeric_limits<double>::max();
-    bool   added_edge    = false;
+    bool added_edge = false;
+
     for( const auto &range_msg : range_queue ) {
-        for( const auto &keyframe : keyframes ) {
-            double time_diff = std::abs( ( rclcpp::Time( range_msg->header.stamp ) - rclcpp::Time( keyframe->stamp ) ).seconds() );
-            if( time_diff < min_time_diff ) {
-                min_time_diff = time_diff;
-            }
-            if( time_diff < ranging_max_time_diff ) {
-                // if( std::abs( ( rclcpp::Time( range_msg->header.stamp ) - rclcpp::Time( keyframe->stamp ) ).seconds() )
-                //     < ranging_max_time_diff ) {
-                const std::string &neighbor_name = range_msg->neighbor_name;
-                RCLCPP_INFO_STREAM( node->get_logger(), "Ranging message from " << range_msg->self_name << " to " << neighbor_name
-                                                                                << " close to " << keyframe->readable_id );
-                if( ranging_data_map.find( neighbor_name ) == ranging_data_map.end() || !ranging_data_map[neighbor_name].initialized ) {
-                    RCLCPP_WARN_STREAM( node->get_logger(), "No information for " << neighbor_name << ", cannot add ranging edge" );
-                    continue;
-                }
-
-
-                double          range      = range_msg->bias_compensation_valid ? range_msg->range_compensated : range_msg->range_raw;
-                Eigen::MatrixXd inf_matrix = Eigen::MatrixXd::Identity( 1, 1 );
-                inf_matrix( 0, 0 )         = range_msg->bias_compensation_valid ? ( 1 / range_msg->var_range_compensated )
-                                                                                : ( 1 / range_msg->var_range_raw );
-                RCLCPP_INFO_STREAM( node->get_logger(), "Adding ranging edge from " << range_msg->self_name << " to " << neighbor_name
-                                                                                    << " with range " << range << " and inf "
-                                                                                    << inf_matrix.value() );
-                graph_slam->add_se3_ranging_edge( keyframe->node, ranging_data_map[neighbor_name].se3_vertex, range, inf_matrix );
-
-                added_edge = true;
-            }
+        if( ranging_data_map.find( range_msg->neighbor_name ) == ranging_data_map.end()
+            || !ranging_data_map[range_msg->neighbor_name].initialized ) {
+            RCLCPP_WARN_STREAM( node->get_logger(), "No information for " << range_msg->neighbor_name << ", cannot add ranging edge" );
+            continue;
+        }
+        KeyFrame::Ptr closest_keyframe = find_closest_keyframe( range_msg, keyframes );
+        if( closest_keyframe != nullptr ) {
+            add_ranging_edge( graph_slam, range_msg, closest_keyframe );
+            added_edge = true;
         }
     }
-
-    RCLCPP_INFO_STREAM( node->get_logger(), "Min time diff: " << min_time_diff );
-    RCLCPP_INFO_STREAM( node->get_logger(), "Added edge: " << added_edge );
 
     if( added_edge ) {
         range_queue.clear();
@@ -126,6 +104,7 @@ RangingProcessor::init_positions( std::shared_ptr<GraphSLAM> &graph_slam )
 
         // TODO: for now, only use the latest position, for stationary boxes this is fine, for other moving robots we need to choose the
         // position corresponding to the ranging message that will be added to the graph
+        // Update the position of the ranging device and prior edge
         auto position_msg = position_deque.back();
 
         Eigen::Isometry3d pose = Eigen::Isometry3d::Identity();
@@ -147,6 +126,44 @@ RangingProcessor::init_positions( std::shared_ptr<GraphSLAM> &graph_slam )
 
         position_deque.clear();
     }
+}
+
+KeyFrame::Ptr
+RangingProcessor::find_closest_keyframe( ros2_radio_ranging_interfaces::msg::Range::ConstSharedPtr range_msg,
+                                         const std::vector<KeyFrame::Ptr>                         &keyframes )
+{
+    KeyFrame::Ptr closest_keyframe = nullptr;
+    double        min_time_diff    = std::numeric_limits<double>::max();
+
+    for( const auto &keyframe : keyframes ) {
+        double time_diff = std::abs( ( rclcpp::Time( range_msg->header.stamp ) - rclcpp::Time( keyframe->stamp ) ).seconds() );
+        if( time_diff < min_time_diff && time_diff < ranging_max_time_diff ) {
+            min_time_diff    = time_diff;
+            closest_keyframe = keyframe;
+        }
+    }
+
+    return closest_keyframe;
+}
+
+void
+RangingProcessor::add_ranging_edge( std::shared_ptr<GraphSLAM>                               &graph_slam,
+                                    ros2_radio_ranging_interfaces::msg::Range::ConstSharedPtr range_msg, KeyFrame::Ptr keyframe )
+{
+    RCLCPP_INFO_STREAM( node->get_logger(), "Ranging message from " << range_msg->self_name << " to " << range_msg->neighbor_name
+                                                                    << " close to " << keyframe->readable_id );
+    double          range      = range_msg->bias_compensation_valid ? range_msg->range_compensated : range_msg->range_raw;
+    Eigen::MatrixXd inf_matrix = Eigen::MatrixXd::Identity( 1, 1 );
+    inf_matrix( 0, 0 ) = range_msg->bias_compensation_valid ? ( 1 / range_msg->var_range_compensated ) : ( 1 / range_msg->var_range_raw );
+    RCLCPP_INFO_STREAM( node->get_logger(), "Adding ranging edge from " << range_msg->self_name << " to " << range_msg->neighbor_name
+                                                                        << " with range " << range << " and inf " << inf_matrix.value() );
+    g2o::EdgeSE3Ranging *g2o_ranging_edge = graph_slam->add_se3_ranging_edge( keyframe->node,
+                                                                              ranging_data_map[range_msg->neighbor_name].se3_vertex, range,
+                                                                              inf_matrix );
+    ranging_data_map[range_msg->neighbor_name].edges_se3_ranging.push_back( g2o_ranging_edge );
+
+    graph_slam->add_robust_kernel( static_cast<g2o::HyperGraph::Edge *>( g2o_ranging_edge ), ranging_edge_robust_kernel,
+                                   ranging_edge_robust_kernel_size );
 }
 
 }  // namespace mrg_slam
